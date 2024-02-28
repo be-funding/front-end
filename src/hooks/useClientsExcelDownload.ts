64896c35c41edaea24e97389e3c2d4610e6282e5
WilferSalas <sony634@hotmail.com>
2024-02-27 20:36:30 -0500

    fix download excel

--- src/hooks/useClientsExcelDownload.ts
+++ src/hooks/useClientsExcelDownload.ts
@@ -14,8 +14,13 @@ export const useClientsExcelDownload = ({
   name
 }: ClientsExcelDownloadProps): { handleDownloadExcel: () => void } => {
   const handleDownloadExcel = () => {
+    const filteredKeys = filteredData.map(data => {
+      const { _id, __v, ...rest } = data;
+      return rest;
+    });
+
     const wb = XLSX.utils.book_new();
-    const ws = XLSX.utils.json_to_sheet(filteredData);
+    const ws = XLSX.utils.json_to_sheet(filteredKeys);
     XLSX.utils.book_append_sheet(wb, ws, name);
     const filename = `${name}_${new Date().toISOString().split('T')[0]}.xlsx`;
     XLSX.writeFile(wb, filename);
