1ef359135da7a0a3e2677d78e6a4226d0bede9b8
WilferSalas <sony634@hotmail.com>
2024-02-29 11:52:19 -0500

    remove metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from '@mui/material/styles';
 import { usePathname } from 'next/navigation';
 
-import type { Metadata } from 'next'
-
 // @components
 import Navigation from '@/components/Navigation';
 
@@ -20,11 +18,6 @@ import theme from '@/theme';
 // @interfaces
 interface LayoutProps {
   children: React.ReactNode;
-}
-
-export const metadata = {
-  title: '...',
-  description: '...',
 }
 
 export default function RootLayout({ children }: LayoutProps) {
